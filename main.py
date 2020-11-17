94137352095c509836edfe2dc5e1cc2d927c6ba2
Antonio Donnarumma <tony_98d@live.it>
2020-11-17 14:53:29 +0100

    Aggiunta queue

--- main.py
+++ main.py
@@ -3,21 +3,52 @@ from EpriceScraper import EpriceScraper
 from MediaworldScraper import MediaworldScraper
 from utility.DatabaseManager import DatabaseManager
 from multiprocessing import Process
+from multiprocessing import Queue
+import time
+from multiprocessing import JoinableQueue
 
 
-def startscrape(scraper, **kwargs):
-    print("Scraper ", scraper, " inizializzato")
-    scrapertype = type(scraper)
-    prodotti = scraper.get_offers()
-    DatabaseManager.insert(prodotti, scrapertype)
-    print("Scraping di ", scrapertype, " eseguito correttamente")
+def startscrape(jobs, report):
+    while True:
+        scraper = jobs.get()
+        print("Scraper ", scraper, " inizializzato")
+        scrapertype = type(scraper)
+        prodotti = scraper.get_offers()
+        print("Scraping di ", scrapertype, " eseguito correttamente")
+        report.put((prodotti, scrapertype))
+        jobs.task_done()
+
+
+def create_process(jobs, report):
+    for _ in range(4):
+        process = Process(target=startscrape, args=(jobs, report))
+        process.daemon = True
+        process.start()
+
+
+def create_jobs(scrapers, jobs):
+    for scraper in scrapers:
+        jobs.put(scraper)
+
+
+def waitForComplete(jobs, report):
+    jobs.join()
+
+    while not report.empty():
+        tupla = report.get_nowait()
+        DatabaseManager.insert(tupla[0], tupla[1])
 
 
 if __name__ == "__main__":
     scrapers = [AmazonScraper(), EpriceScraper(), MediaworldScraper()]
     #scrapers = [AmazonScraper()]
-    for scraper in scrapers:
-        p = Process(target=startscrape, args=(scraper,))
-        p.start()
+    report = JoinableQueue()
+    jobs = JoinableQueue()
+
+    create_process(jobs, report)
+
+    create_jobs(scrapers, jobs)
+    time.sleep(2)
+    waitForComplete(jobs, report)
 
 
